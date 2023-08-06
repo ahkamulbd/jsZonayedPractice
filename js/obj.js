d1ca6303738c31a4aba19567de2dcd88e7a0f8f4
ahkamulbd <ahkamulbd@gmail.com>
2023-08-06 20:32:13 +0600

    JS Object addressed

--- js/obj.js
+++ js/obj.js
@@ -1,4 +1,27 @@
-let abdurRahim = {
-    fullName: 'Abdur Rahim',
+// let abdurRahim = {
+//     fullName: 'Abdur Rahim',
+// }
 
+let studentObject = {
+    school: 'Chakaria Korak Biddapith',
+    class: 3,
+    books: ['Bangla', 'English', 'Math', 'Religion', 'Science', 'BGS'],
+    schoolDress: ['Blue Skirt', 'Deep Blue Goun', 'White Shoe']
 }
+
+// console.log(studentObject.books[3]);
+// console.log(studentObject.schoolDress[1]);
+
+// Array Object:
+
+let arrObj = [
+    'Amima',
+    {
+        name: 'Arkam',
+        age: 5,
+        job: 'Student'
+    },
+    'Bangladesh'
+]
+//console.log(arrObj[0]);
+console.log(arrObj[1].job);
