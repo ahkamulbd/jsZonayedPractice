552024bc828edd4bb7f89130795248aa9d3517e3
ahkamulbd <ahkamulbd@gmail.com>
2023-08-06 16:29:16 +0600

    codes on array added

---
+++ js/array.js
@@ -0,0 +1,10 @@
+const nameArray = ['Shomi', 'Rumi', 'Promi', 'Joni', 'Moni', 'Honey', 'Koni'];
+
+let arrayLength = nameArray.length;
+
+for (let i = 0; i < arrayLength; i++) {
+    let item = nameArray[i];
+    //console.log(i, item);
+    //console.log(`${item}'s index is ${i}`);
+}
+console.log(nameArray);
